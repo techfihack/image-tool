b2ffc1c8c55fc1913c1c7c78f68bfa749d923573
junyin.chan <junyin.chan@techfi.io>
2024-03-07 19:02:07 +0800

    delete all

--- src/main/java/com/example/imageprocesspip/service/ImageStorageService.java
+++
@@ -1,8 +0,0 @@
-package com.example.imageprocesspip.service;
-
-import java.io.IOException;
-
-public interface ImageStorageService {
-    void saveImage(String path, byte[] data) throws IOException;
-    byte[] getImage(String path) throws IOException;
-}
